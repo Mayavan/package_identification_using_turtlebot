b127cfde1592044c33010a45e744097aab4f0a04
mayavan <mayavan95@gmail.com>
2018-12-02 23:41:27 -0500

    [Task 2.5][QReader.cpp] Added refined verification of detected finder pattern. When the finder pattern is confirmed, the center point and module size of the finder pattern is added to the vector. Driver - RajendraMayavan Navigator - Adarsh Jagan

--- include/Package_Identification_using_Turtlebot/QReader.hpp
+++ include/Package_Identification_using_Turtlebot/QReader.hpp
@@ -19,6 +19,12 @@ class QReader {
   std::vector<float> estimatedModuleSize;
 
   bool checkRatio(std::vector<int>);
+  bool isFinderPattern(const cv::Mat&, std::vector<int>, int, int);
+  float checkVertical(const cv::Mat&, int, int, int, int);
+  float checkHorizontal(const cv::Mat&, int, int, int, int);
+  bool checkDiagonal(const cv::Mat&, float, float, int, int);
+
+  float columnCenterEstimate(std::vector<int>, int);
   cv::Mat warpToCode(cv::Mat);
   std::vector<uint8_t> extractBits(cv::Mat);
   std::vector<char> decodeBits(std::vector<uint8_t>);
--- src/QReader.cpp
+++ src/QReader.cpp
@@ -75,7 +75,7 @@ cv::Mat QReader::captureImage() {
  *
  * @return true if the image has a QR code in it, else returns false
  */
-bool QReader::checkQCodeExists(cv::Mat &img) {
+bool QReader::checkQCodeExists(cv::Mat& img) {
   possibleCenters.clear();
   estimatedModuleSize.clear();
 
@@ -85,7 +85,7 @@ bool QReader::checkQCodeExists(cv::Mat &img) {
   for (int row = skipRows - 1; row < img.rows; row += skipRows) {
     stateCount = {0, 0, 0, 0, 0};
     currentState = 0;
-    const uchar *ptr = img.ptr<uchar>(row);
+    const uchar* ptr = img.ptr<uchar>(row);
     for (int col = 0; col < img.cols; col++) {
       if (ptr[col] < 128) {
         // Found Black Pixel
@@ -110,11 +110,9 @@ bool QReader::checkQCodeExists(cv::Mat &img) {
             // Check if the previous state count satisfies the expected ratio
             // Do processing for it here
             if (checkRatio(stateCount)) {
-              // Do more checks is see if it actually is a finder pattern
-
-              // Dummy implementation to check algorithm
-              possibleCenters.push_back(cv::Point2f(0.3f, 0.f));
-
+              // Do more checks is see if it actually is a finder pattern and
+              // add the position and module size of the patterns
+              isFinderPattern(img, stateCount, row, col);
             } else {
               // If the ratio is not satisfied, swift the states two step front
               currentState = 3;
@@ -145,14 +143,15 @@ bool QReader::checkQCodeExists(cv::Mat &img) {
 /**
  * @brief checks if the number of pixels in each states are in the ratio
  * 1:1:3:1:1
- * @param stateCount containing the number of pixels with black or white pixels
+ * @param stateCount vector containing the number of pixels with black or white
+ * pixels
  * @return true if the number of pixels in each states are in the ratio
  * 1:1:3:1:1 or false
  */
 bool QReader::checkRatio(std::vector<int> stateCount) {
   int totalCount = 0;
   // find the total cols in stateCount
-  for (auto &count : stateCount) {
+  for (auto& count : stateCount) {
     totalCount += count;
   }
 
@@ -170,7 +169,333 @@ bool QReader::checkRatio(std::vector<int> stateCount) {
           (abs(moduleSize - (stateCount[4])) < tolerance));
 }
 
-cv::Mat warpToCode(cv::Mat) {}
-std::vector<uint8_t> extractBits(cv::Mat) {}
+/**
+ * @brief Ensure if the given part of the image has finder pattern
+ *
+ * @param img input image
+ * @param stateCount vector of number of pixels in each transitions from white
+ * to black or black to white pixels
+ * @param row row number of the pixel in the image
+ * @param col column number of the pixel in the image
+ *
+ * @return false if the row or column goes out of limits or true if it is not
+ */
+bool QReader::isFinderPattern(const cv::Mat& img, std::vector<int> stateCount,
+                              int row, int col) {
+  int totalCount = 0;
+  for (auto& count : stateCount) {
+    totalCount += count;
+  }
+  // Get a rough estimate of center
+  float centerCol = columnCenterEstimate(stateCount, col);
 
-std::vector<char> decodeBits(std::vector<uint8_t>) {}
+  // Find the refined center in vertical direction
+  float centerRow =
+      checkVertical(img, row, centerCol, stateCount[2], totalCount);
+  if (std::isnan(centerRow)) {
+    return false;
+  }
+  // Find the refined center in horizontal direction
+  centerCol =
+      checkHorizontal(img, centerRow, centerCol, stateCount[2], totalCount);
+  if (std::isnan(centerCol)) {
+    return false;
+  }
+
+  // Cross check along the diagonal with the new center row and col
+  bool validPattern =
+      checkDiagonal(img, centerRow, centerCol, stateCount[2], totalCount);
+  if (!validPattern) {
+    return false;
+  }
+
+  cv::Point2f ptNew(centerCol, centerRow);
+  float newModuleSize = totalCount / 7.0f;
+  bool found = false;
+  int idx = 0;
+
+  // Check if the finder pattern is already found
+  for (cv::Point2f pt : possibleCenters) {
+    cv::Point2f diff = pt - ptNew;
+    float dist = sqrt(diff.dot(diff));
+    // If the distance between two centers is less than 10px, they're the same.
+    if (dist < 10) {
+      // If two centers are very close by, we improve the estimate of the center
+      // point and module size by taking their means
+      pt = pt + ptNew;
+      pt.x /= 2.0f;
+      pt.y /= 2.0f;
+      estimatedModuleSize[idx] =
+          (estimatedModuleSize[idx] + newModuleSize) / 2.0f;
+      found = true;
+      break;
+    }
+    idx++;
+  }
+  if (!found) {
+    possibleCenters.push_back(ptNew);
+    estimatedModuleSize.push_back(newModuleSize);
+  }
+  return true;
+}
+
+/**
+ * @brief Find the center point in the vertical direction
+ *
+ * @param img input image
+ * @param startRow Row number of the pixel in the image
+ * @param centerCol Estimate of center pixel in the column
+ * @param centerCount Number of pixels in the length of center square
+ * @param totalCount Number of pixels in the length of the whole finder pattern
+ *
+ * @return The center of the finder pattern in the vertical direction
+ */
+float QReader::checkVertical(const cv::Mat& img, int startRow, int centerCol,
+                             int centerCount, int totalCount) {
+  int maxRows = img.rows;
+  std::vector<int> checkStateCount(5, 0);
+  int row = startRow;
+  // Traverse above the center
+  while (row >= 0 && img.at<uchar>(row, centerCol) < 128) {
+    checkStateCount[2]++;
+    row--;
+  }
+  if (row < 0) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (row >= 0 && img.at<uchar>(row, centerCol) >= 128 &&
+         checkStateCount[1] < centerCount) {
+    checkStateCount[1]++;
+    row--;
+  }
+  if (row < 0 || checkStateCount[1] >= centerCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (row >= 0 && img.at<uchar>(row, centerCol) < 128 &&
+         checkStateCount[0] < centerCount) {
+    checkStateCount[0]++;
+    row--;
+  }
+  if (row < 0 || checkStateCount[0] >= centerCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  // Traverse down the center
+  row = startRow + 1;
+  while (row < maxRows && img.at<uchar>(row, centerCol) < 128) {
+    checkStateCount[2]++;
+    row++;
+  }
+  if (row == maxRows) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (row < maxRows && img.at<uchar>(row, centerCol) >= 128 &&
+         checkStateCount[3] < centerCount) {
+    checkStateCount[3]++;
+    row++;
+  }
+  if (row == maxRows || checkStateCount[3] >= totalCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (row < maxRows && img.at<uchar>(row, centerCol) < 128 &&
+         checkStateCount[4] < centerCount) {
+    checkStateCount[4]++;
+    row++;
+  }
+  if (row == maxRows || checkStateCount[4] >= centerCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  int totalCheckcount = 0;
+  for (auto& count : checkStateCount) {
+    totalCheckcount += count;
+  }
+
+  if (5 * abs(totalCheckcount - totalCount) >= 2 * totalCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  double centerRow = columnCenterEstimate(checkStateCount, row);
+  if (checkRatio(checkStateCount)) {
+    return centerRow;
+  } else {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+}
+
+/**
+ * @brief Find the center point in the horizontal direction
+ *
+ * @param img input image
+ * @param centerRow Center of the finder pattern in the vertical direction
+ * @param startCol Column number of the pixel in the image
+ * @param centerCol Estimate of center pixel in the column
+ * @param centerCount Number of pixels in the length of center square
+ * @param totalCount Number of pixels in the length of the whole finder pattern
+ *
+ * @return The center of the finder pattern in the horizontal direction
+ */
+float QReader::checkHorizontal(const cv::Mat& img, int centerRow, int startCol,
+                               int centerCount, int totalCount) {
+  int maxCols = img.cols;
+  std::vector<int> stateCount(5, 0);
+  int col = startCol;
+  const uchar* ptr = img.ptr<uchar>(centerRow);
+  // traverse towards the left of finder pattern
+  while (col >= 0 && ptr[col] < 128) {
+    stateCount[2]++;
+    col--;
+  }
+  if (col < 0) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (col >= 0 && ptr[col] >= 128 && stateCount[1] < centerCount) {
+    stateCount[1]++;
+    col--;
+  }
+  if (col < 0 || stateCount[1] == centerCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (col >= 0 && ptr[col] < 128 && stateCount[0] < centerCount) {
+    stateCount[0]++;
+    col--;
+  }
+  if (col < 0 || stateCount[0] == centerCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  col = startCol + 1;
+  // traverse towards the right of finder pattern
+  while (col < maxCols && ptr[col] < 128) {
+    stateCount[2]++;
+    col++;
+  }
+  if (col == maxCols) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (col < maxCols && ptr[col] >= 128 && stateCount[3] < centerCount) {
+    stateCount[3]++;
+    col++;
+  }
+  if (col == maxCols || stateCount[3] == centerCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  while (col < maxCols && ptr[col] < 128 && stateCount[4] < centerCount) {
+    stateCount[4]++;
+    col++;
+  }
+  if (col == maxCols || stateCount[4] == centerCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  int totalCheckcount = 0;
+  for (auto& count : stateCount) {
+    totalCheckcount += count;
+  }
+
+  if (5 * abs(totalCount - totalCheckcount) >= totalCount) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  double centerCol = columnCenterEstimate(stateCount, col);
+  if (checkRatio(stateCount)) {
+    return centerCol;
+  } else {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+}
+
+/**
+ * @brief Find the center point in the horizontal direction
+ *
+ * @param img input image
+ * @param centerRow Center of the finder pattern in the vertical direction
+ * @param centerCol Center of the finder pattern in the horizontal direction
+ * @param maxCount Number of pixels in the length of center square
+ * @param totalCount Number of pixels in the length of the whole finder pattern
+ *
+ * @return The center of the finder pattern in the horizontal direction
+ */
+bool QReader::checkDiagonal(const cv::Mat& img, float centerRow,
+                            float centerCol, int maxCount, int totalCount) {
+  std::vector<int> stateCount(5, 0);
+  int col = centerCol;
+  int row = centerRow;
+  // traverse towards the top left corner
+  while (col >= 0 && row >= 0 && img.at<uchar>(row, col) < 128) {
+    stateCount[2]++;
+    row--;
+    col--;
+  }
+  if (row < 0 || col < 0) {
+    return false;
+  }
+  while (row >= 0 && col >= 0 && img.at<uchar>(row, col) >= 128 &&
+         stateCount[1] <= maxCount) {
+    stateCount[1]++;
+    row--;
+    col--;
+  }
+  if (row < 0 || col < 0 || stateCount[1] > maxCount) {
+    return false;
+  }
+  while (row >= 0 && col >= 0 && img.at<uchar>(row, col) < 128 &&
+         stateCount[0] <= maxCount) {
+    stateCount[0]++;
+    row--;
+    col--;
+  }
+  if (stateCount[0] > maxCount) {
+    return false;
+  }
+  int maxCols = img.cols;
+  int maxRows = img.rows;
+  col = centerCol + 1;
+  row = centerRow + 1;
+  // traverse towards the bottom right corner
+  while (row < maxRows && col < maxCols && img.at<uchar>(row, col) < 128) {
+    stateCount[2]++;
+    row++;
+    col++;
+  }
+  if (row >= maxRows || col >= maxCols) {
+    return false;
+  }
+  while (row < maxRows && col < maxCols && img.at<uchar>(row, col) >= 128 &&
+         stateCount[3] < maxCount) {
+    stateCount[3]++;
+    row++;
+    col++;
+  }
+  if (row >= maxRows || col >= maxCols || stateCount[3] > maxCount) {
+    return false;
+  }
+  while (row < maxRows && col < maxCols && img.at<uchar>(row, col) < 128 &&
+         stateCount[4] < maxCount) {
+    stateCount[4]++;
+    row++;
+    col++;
+  }
+  if (row >= maxRows || col >= maxCols || stateCount[4] > maxCount) {
+    return false;
+  }
+  int newTotalcount = 0;
+  for (auto& count : stateCount) {
+    newTotalcount += count;
+  }
+  bool possible = (abs(totalCount - newTotalcount) < 2 * totalCount) &&
+                  checkRatio(stateCount);
+  // return true if ratio holds
+  return possible;
+}
+
+/**
+ * @brief Calculate center center pixel in the column of finder pattern
+ * @param stateCount Vector containing the number of pixels with black or white
+ * pixels
+ * @param end Last column
+ * @return Estimate of the center of the finder pattern in the column
+ */
+float QReader::columnCenterEstimate(std::vector<int> stateCount, int end) {
+  float mid = (end - stateCount[4] - stateCount[3]) - stateCount[2] / 2.0f;
+  return mid;
+}
+
+cv::Mat QReader::warpToCode(cv::Mat) {}
+std::vector<uint8_t> QReader::extractBits(cv::Mat) {}
+
+std::vector<char> QReader::decodeBits(std::vector<uint8_t>) {}
